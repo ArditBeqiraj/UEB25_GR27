e58375d0f761ee92f58f380c2cb7b7a76ef35bd3
Bledion Zymberi <Bledion.zymberi@student.uni-pr.edu>
2025-05-20 15:48:11 +0200

    Sign-up logic, shtimi i te dhenave ne db

--- UEB24_GR24/member/validate_signup.php
+++ UEB24_GR24/member/validate_signup.php
@@ -1,5 +1,23 @@
 <?php
 require_once 'popup-functions.php';
+require '../admin/db.php';
+
+function random_num($length)
+{
+
+    $text = "";
+    if ($length < 5) {
+        $length = 5;
+    }
+
+    $len = rand(4, $length);
+
+    for ($i = 0; $i < $len; $i++) {
+        $text .= rand(0, 9);
+    }
+
+    return $text;
+}
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = $_POST["name"] ?? "";
@@ -7,6 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = $_POST["username"] ?? "";
     $email = $_POST["email"] ?? "";
     $password = $_POST["password"] ?? "";
+    $comfirm_password = $_POST["comfirm-password"] ?? "";
 
     if (validateName($name) !== 1 || validateName($surname) !== 1) {
         echo "Name and Surname should only contain letters.";
@@ -27,4 +46,56 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo "Password must be at least 8 characters and contain at least one number.";
         return;
     }
+
+    if ($password != $comfirm_password) {
+        die("Password should match");
+    }
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    $check_query = "SELECT * FROM users WHERE username='$username' OR email='$email'";
+    $check_result = mysqli_query($conn, $check_query);
+
+    if (mysqli_num_rows($check_result) > 0) {
+        die("Username ose email tashmë ekziston.");
+    }
+
+
+
+    $user_id = random_num(20);
+    $sql = "INSERT INTO users (name, surname, user_id, username, email, password) 
+            VALUES ('$name', '$surname', '$user_id', '$username', '$email', '$hashed_password')";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: validate_signup.php");
+    } else {
+        echo "Gabim: " . mysqli_error($conn);
+    }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kyqja</title>
+</head>
+
+
+<body>
+
+    <h2>You sign up succesfuly, please <a href="../index.php#log-in" id="log-inOrsign-up">log in</a>
+    </h2>
+
+
+
+</body>
+
+
+
+
+</html>
