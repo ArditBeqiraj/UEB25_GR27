7a8d1639399633360628883794b9ddee4bf9d3f5
Ardit Beqiraj <ardit.beqiraj2005@gmail.com>
2025-05-21 15:22:34 +0200

    Update validate log in

--- UEB24_GR24/member/validate_login.php
+++ UEB24_GR24/member/validate_login.php
@@ -1,18 +1,35 @@
 <?php
-require_once 'functions.php';
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"] ?? "";
-    $password = $_POST["password"] ?? "";
+require '../admin/db.php';
 
-    if (!validateEmail($email)) {
-        echo "Invalid email format.";
-        return;
+
+session_start();
+require '../admin/db.php'; // lidhja me databazën
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (!empty($email) && !empty($password) && !is_numeric($email)) {
+
+        //read from database
+        $query = "select * from users where email = '$email' limit 1";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            if ($result && mysqli_num_rows($result) > 0) {
+
+                $user_data = mysqli_fetch_assoc($result);
+
+                if ($user_data['password'] === $password) {
+
+                    $_SESSION['user_id'] = $user_data['user_id'];
+                }
+            }
+        }
+        header("Location: ../admin/dashboard.php");
+        die;
+    } else {
+        echo "wrong username or password!";
     }
-
-    if (!validatePassword($password)) {
-        echo "Password must be at least 8 characters and contain at least one number.";
-        return;
-    }
- }
-?>
+}
